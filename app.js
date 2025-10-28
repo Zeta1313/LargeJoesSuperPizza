6cd7751644228bde408e1a59ea548eafdc47ecb0
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-10-28 11:02:36 -0700

    Update for 10/28

--- app.js
+++ app.js
@@ -2,14 +2,55 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true}));
+
+const orders = [];
+
 const PORT = 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     //res.send('Large Joes Super Pizza is currently closed due to health violations');
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 });
+
+app.get('/contact-us', (req, res) => {
+    //res.send('Large Joes Super Pizza is currently closed due to health violations');
+    res.render('contact');
+});
+
+app.get('/confirm', (req, res) => {
+    //res.send('Large Joes Super Pizza is currently closed due to health violations');
+    res.render('confirmation');
+});
+
+app.get('/admin', (req, res) => {
+    //res.send('Large Joes Super Pizza is currently closed due to health violations');
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    res.send(orders);
+});
+
+app.post('/submit-order', (req, res) => {
+    //res.send('Large Joes Super Pizza is currently closed due to health violations');
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+    //console.log(req.body);
+
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        pnum: req.body.pnum,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size
+    }
+    orders.push(order);
+    console.log(order);
+    res.render('confirmation', { name: order.fname});
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`) ;
--- public/js/pizza.js
+++ public/js/pizza.js
@@ -1,26 +1,33 @@
 document.getElementById('pizza-form').onsubmit = () => {
-clearErrors();
-let isvalid = true;
+    alert ("testing");
+    clearErrors();
+    let isvalid = true;
 
-let fname = document.getElementById('fname').value.trim();
-if(fname=="") {
-    document.getElementById("er-fname").style.display="block";
-    isvalid = false;
-}
+    let fname = document.getElementById('fname').value.trim();
+    if(fname=="") {
+        document.getElementById("er-fname").style.display="block";
+        isvalid = false;
+    }
 
-let lname = document.getElementById('lname').value.trim();
-if(lname=="") {
-    document.getElementById("er-lname").style.display="block";
-    isvalid = false;
-}
+    let lname = document.getElementById('lname').value.trim();
+    if(lname=="") {
+        document.getElementById("er-lname").style.display="block";
+        isvalid = false;
+    }
 
-let email = document.getElementById('email').value.trim();
-if (!email || email.value.indexOf("@") == -1) {
-    document.getElementById("er-email").style.display="block";
-    isvalid=false;
-}
+    let email = document.getElementById('email').value.trim();
+    if (!email || email.value.indexOf("@") == -1) {
+        document.getElementById("er-email").style.display="block";
+        isvalid=false;
+    }
 
-return isvalid;
+    let size = document.getElementById('size').value;
+    if (size === "none") {
+            document.getElementById("err-size").style.display = "block";
+            isvalid = false;
+    }
+
+    return isvalid;
 }
 
 function clearErrors() {
